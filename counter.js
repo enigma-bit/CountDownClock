1b3fd5d8c14c54a9bdde255d2ae76d9cf62eee38
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-02-16 17:25:50 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,59 +1,24 @@
-Even = [0,]
-Odd = [0,]
-source = []
+evenNumb = []
+oddNumb = []
 
-function StartEven() {
-    setInterval(SeparateEven, 1000);
+function EvenNumbers() {
+
+for (let i = 1; i < 100; i++) {
+    if (i % 2 == 0) {
+
+        evenNumb.push(i);
+        document.getElementById("counterEven").innerHTML = evenNumb;
+    } 
+  }
 }
 
-function StartOdd() {
-    setInterval(SeparateOdd, 1000);
-}
 
-function reset() {
-    Even.length = 0;
-    Odd.length = 0;
-    source.length = 0;
-}
+function OddNumbers() {
 
-function SeparateEven() {
-
-    index = 0;
-    
-    while (index < source.length) {
-        if ((source[index]%2)==0) {
-
-            even.push(source[index]);       
-            
-            
-        } else {
-
-            newNumb = source[index]+1;
-            source.push(newNumb);
-        }
-
-    index++;
-    document.getElementById("counter").innerHTML = Even;
-
-    }    
-}
-
-function SeparateOdd() {
-    
-    index = 0;
-    
-    while (index < source.length) {
-        if ((source[index]%2)!=0) {
-            
-            Odd.push(index);
-            
-        }else {
-
-            newNumb = source[index]+1;
-            source.push(newNumb);
-        }
-    index++;
-    document.getElementById("counter").innerHTML = Odd;
-
-    }    
+for (let i = 1; i < 100; i++) {
+    if (i % 2 != 0) {
+        oddNumb.push(i);
+        document.getElementById("counterOdd").innerHTML = oddNumb;
+    } 
+  }
 }
