96bcd4a93113f4d037aaa0d905d463ac66416ec6
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-02-18 09:44:11 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -8,11 +8,11 @@ function myFunction() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("clock").innerHTML = "Time-Up";
+    document.getElementById("countdown").innerHTML = "Time-Up";
   }
 }, 1000);
   
