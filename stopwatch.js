5e6d0222023f262cbd8d788aa9d2a205471950c1
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-02-27 12:24:52 +0200

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,11 +1,32 @@
 var Addsec
 var AddsecValue= Addsec
 const laps = []
-let i = 0;
+var seconds = 00;
+var minutes = 00;
+var hours = 00;
 
 function myTimer() {
-  document.getElementById("stopwatch").innerHTML = i;
-  i++;
+  seconds++;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("hours").innerHTML = hours;
+
+  if (seconds==60) {
+
+    seconds=0;
+    document.getElementById("seconds").innerHTML = seconds;
+    minutes= minutes +1;
+    document.getElementById("minutes").innerHTML = minutes;
+  }
+
+  if (minutes==60) {
+
+    minutes=0;
+    document.getElementById("minutes").innerHTML = minutes;
+    hours = hours + 1;
+    document.getElementById("hours").innerHTML = hours;
+
+  }
 }
 
 function start() {
@@ -15,25 +36,30 @@ Addsec = setInterval(myTimer, 1000);
 function stop() {
 clearInterval(Addsec);
 Addsec = null;
-document.getElementById("stopwatch").innerHTML = i;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
 }
 
 function reset() {
 clearInterval(Addsec);
 Addsec = null;
 Addsec = setInterval(myTimer, 0);
-i=0;
-laps.length=0;
 }
 
 function displayReset() {
-i = 0;
+hours=0;
+seconds=0;
+minutes=0;
 laps.length=0;
 document.getElementById("laprec").innerHTML = laps;
-document.getElementById("stopwatch").innerHTML = i;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
 }
 
 function lap () {    
-    laps.push(i);
-    document.getElementById("laprec").innerHTML = laps; 
+    laps.push(['[',hours,minutes,seconds,']']);
+    document.getElementById("laprec").innerHTML = laps 
   }
