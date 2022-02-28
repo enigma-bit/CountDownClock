9e8a3823708768dca7c9eb796f8cde2a8cbaa07b
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-02-28 15:23:07 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,20 +2,24 @@ function myFunction() {
   var timeC = new Date();
   var timeU = document.getElementById("time").value;
 
-  const [StrhourU, StrminuteU] = timeU.split(':');
+  const [StrhourU, StrminuteU, Strseconds] = timeU.split(':');
   var hourU = parseInt(StrhourU);
   var minuteU = parseInt(StrminuteU);
+  var secondsU = parseInt(Strseconds) ;
 
   var hourC = timeC.getHours();
   var minuteC = timeC.getMinutes();
+  var secondsC = timeC.getSeconds();
 
   var distanceHour = hourU - hourC ;
   var distanceMin = minuteU - minuteC ;
+  var distanceSec = secondsU - secondsC ;
 
-
-  document.getElementById("countdown1").innerHTML = distanceHour + " Hours";
+  document.getElementById("countdown1").innerHTML = distanceHour + " Hours" ;
+  document.getElementById("countdown2").innerHTML = distanceMin + " Minutes" ;
   document.getElementById("and").innerHTML = "and" ;
-  document.getElementById("countdown2").innerHTML = distanceMin + " and Minutes left";
+  document.getElementById("countdown3").innerHTML = distanceSec + " Seconds left" ;
+
 
 
   if (distanceHour<0 || distanceMin<0) {
