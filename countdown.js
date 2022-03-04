3b0431498edf1d82b1293015b7f8523de7db0c4e
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-03-04 16:11:40 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -3,9 +3,7 @@ function myFunction() {
   var timeU = document.getElementById("time").value;
 
   if (timeU.length === 0) {
-
     document.getElementById("countdown1").innerHTML = "Please enter a time";
-
   }
 
   else {
@@ -27,26 +25,23 @@ function myFunction() {
       distanceHour = distanceHour - 1 ;
       distanceMin = distanceMin + 60 ;
     }
-  if (distanceSec<0) {
-    distanceMin = distanceMin - 1;
-    distanceSec = distanceSec + 60;
-  }
-  
-  document.getElementById("countdown1").innerHTML = distanceHour + " Hours" ;
-  document.getElementById("countdown2").innerHTML = distanceMin + " Minutes" ;
-  document.getElementById("and").innerHTML = "and" ;
-  document.getElementById("countdown3").innerHTML = distanceSec + " Seconds left" ;
-  
-  
-  
+
+    if (distanceSec<0) {
+      distanceMin = distanceMin - 1;
+      distanceSec = distanceSec + 60;
+    }
+
+    document.getElementById("countdown1").innerHTML = distanceHour + " Hours" ;
+    document.getElementById("countdown2").innerHTML = distanceMin + " Minutes" ;
+    document.getElementById("and").innerHTML = "and" ;
+    document.getElementById("countdown3").innerHTML = distanceSec + " Seconds left" ;
+    
     if (distanceHour<0 || distanceMin<0 || distanceSec<0) {
       var msg = new SpeechSynthesisUtterance();
       msg.text = "Time is up";
       window.speechSynthesis.speak(msg);
       document.getElementById("countdown").innerHTML = "Time-Up";
     }
-
-    
   }
 }
 
