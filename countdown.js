0d0d347ab2cf5054efc7dd4f3cf5800834c73a36
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-03-04 16:05:18 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -2,39 +2,51 @@ function myFunction() {
   var timeC = new Date();
   var timeU = document.getElementById("time").value;
 
-  const [StrhourU, StrminuteU, Strseconds] = timeU.split(':');
-  var hourU = parseInt(StrhourU);
-  var minuteU = parseInt(StrminuteU);
-  var secondsU = parseInt(Strseconds) ;
+  if (timeU.length === 0) {
 
-  var hourC = timeC.getHours();
-  var minuteC = timeC.getMinutes();
-  var secondsC = timeC.getSeconds();
+    document.getElementById("countdown1").innerHTML = "Please enter a time";
 
-  var distanceHour = hourU - hourC ;
-  var distanceMin = minuteU - minuteC ;
-  var distanceSec = secondsU - secondsC ;
-
-  if (distanceMin<0) {
-    distanceHour = distanceHour - 1 ;
-    distanceMin = distanceMin + 60 ;
   }
-if (distanceSec<0) {
-  distanceMin = distanceMin - 1;
-  distanceSec = distanceSec + 60;
-}
+
+  else {
+
+    const [StrhourU, StrminuteU, Strseconds] = timeU.split(':');
+    var hourU = parseInt(StrhourU);
+    var minuteU = parseInt(StrminuteU);
+    var secondsU = parseInt(Strseconds) ;
+  
+    var hourC = timeC.getHours();
+    var minuteC = timeC.getMinutes();
+    var secondsC = timeC.getSeconds();
+  
+    var distanceHour = hourU - hourC ;
+    var distanceMin = minuteU - minuteC ;
+    var distanceSec = secondsU - secondsC ;
+  
+    if (distanceMin<0) {
+      distanceHour = distanceHour - 1 ;
+      distanceMin = distanceMin + 60 ;
+    }
+  if (distanceSec<0) {
+    distanceMin = distanceMin - 1;
+    distanceSec = distanceSec + 60;
+  }
+  
   document.getElementById("countdown1").innerHTML = distanceHour + " Hours" ;
   document.getElementById("countdown2").innerHTML = distanceMin + " Minutes" ;
   document.getElementById("and").innerHTML = "and" ;
   document.getElementById("countdown3").innerHTML = distanceSec + " Seconds left" ;
+  
+  
+  
+    if (distanceHour<0 || distanceMin<0 || distanceSec<0) {
+      var msg = new SpeechSynthesisUtterance();
+      msg.text = "Time is up";
+      window.speechSynthesis.speak(msg);
+      document.getElementById("countdown").innerHTML = "Time-Up";
+    }
 
-
-
-  if (distanceHour<0 || distanceMin<0 || distanceSec<0) {
-    var msg = new SpeechSynthesisUtterance();
-    msg.text = "Time is up";
-    window.speechSynthesis.speak(msg);
-    document.getElementById("countdown").innerHTML = "Time-Up";
+    
   }
 }
 
