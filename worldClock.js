0358ca7552b76009f3083ad1ca73d26d773d5919
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-03-07 11:57:40 +0200

    Update worldClock.js

--- worldClock.js
+++ worldClock.js
@@ -1,12 +1,19 @@
-const harare = new Date().toLocaleString('en-US', { timeZone: 'Africa/Harare' });
-document.getElementById("hre").innerHTML= harare
-const Abidjan = new Date().toLocaleString('en-US', { timeZone: 'Africa/Abidjan' });
-document.getElementById("abj").innerHTML= Abidjan
-const London = new Date().toLocaleString('en-US', { timeZone: 'Europe/London' });
-document.getElementById("lnd").innerHTML= London
-const Amsterdam = new Date().toLocaleString('en-US', { timeZone: 'Europe/Amsterdam' });
-document.getElementById("amst").innerHTML= Amsterdam
-const Bangkok = new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok' });
-document.getElementById("bangk").innerHTML= Bangkok
-const Taiwan = new Date().toLocaleString('en-US', { timeZone: 'Asia/Taipei' });
-document.getElementById("taiwn").innerHTML= Taiwan
+function WorldClockTime() {
+    const harare = new Date().toLocaleString('en-US', { timeZone: 'Africa/Harare' });
+    document.getElementById("hre").innerHTML= harare ;
+    const Abidjan = new Date().toLocaleString('en-US', { timeZone: 'Africa/Abidjan' });
+    document.getElementById("abj").innerHTML= Abidjan;
+    const London = new Date().toLocaleString('en-US', { timeZone: 'Europe/London' });
+    document.getElementById("lnd").innerHTML= London;
+    const Amsterdam = new Date().toLocaleString('en-US', { timeZone: 'Europe/Amsterdam' });
+    document.getElementById("amst").innerHTML= Amsterdam;
+    const Bangkok = new Date().toLocaleString('en-US', { timeZone: 'Asia/Bangkok' });
+    document.getElementById("bangk").innerHTML= Bangkok;
+    const Taiwan = new Date().toLocaleString('en-US', { timeZone: 'Asia/Taipei' });
+    document.getElementById("taiwn").innerHTML= Taiwan;
+}
+
+function refresh() {
+    setInterval(WorldClockTime,200);
+}
+
