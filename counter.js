e6d9fc1b2bdffa2dcbb70dae1a605f06897f0635
enigma-bit <53010263+enigma-bit@users.noreply.github.com>
2022-03-07 16:01:31 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,66 +1,44 @@
-function myFunction() {
-  var timeC = new Date();
-  var timeU = document.getElementById("time").value;
-  const [StrhourU, StrminuteU, Strseconds] = timeU.split(':');
-
-  if (StrhourU.length === 0) {
-    document.getElementById("countdown1").innerHTML = "Please enter hours";
-
-    if (StrminuteU.length === 0) {
-      document.getElementById("countdown1").innerHTML = "Please enter minutes";
-
-      if (Strseconds.length === 0) {
-        document.getElementById("countdown1").innerHTML = "Please enter seconds";
-
-        if (timeU.length === 0) {
-          document.getElementById("countdown1").innerHTML = "Please enter a time";
+var number = 0;
+        function updateNumber() {
+            document.getElementById("clickCount").innerHTML = number;
         }
 
-      }
-    }
-  }
+        function checkNumberEvenOdd() {
 
-  else {
+            
 
-    
-    var hourU = parseInt(StrhourU);
-    var minuteU = parseInt(StrminuteU);
-    var secondsU = parseInt(Strseconds) ;
-  
-    var hourC = timeC.getHours();
-    var minuteC = timeC.getMinutes();
-    var secondsC = timeC.getSeconds();
-  
-    var distanceHour = hourU - hourC ;
-    var distanceMin = minuteU - minuteC ;
-    var distanceSec = secondsU - secondsC ;
-  
-    if (distanceMin<0) {
-      distanceHour = distanceHour - 1 ;
-      distanceMin = distanceMin + 60 ;
-    }
+            if(number % 2 == 0) {
+                var result = "The number is even.";
+                document.getElementById("display").innerHTML = result;
 
-    if (distanceSec<0) {
-      distanceMin = distanceMin - 1;
-      distanceSec = distanceSec + 60;
-    }
+            }else {
+                var result = "The number is odd.";
+                document.getElementById("display").innerHTML = result;
+            }
 
-    document.getElementById("countdown1").innerHTML = distanceHour + " Hours" ;
-    document.getElementById("countdown2").innerHTML = distanceMin + " Minutes" ;
-    document.getElementById("and").innerHTML = "and" ;
-    document.getElementById("countdown3").innerHTML = distanceSec + " Seconds left" ;
-    
-    if (distanceHour<0 || distanceMin<0 || distanceSec<0) {
-      var msg = new SpeechSynthesisUtterance();
-      msg.text = "Time is up";
-      window.speechSynthesis.speak(msg);
-      document.getElementById("countdown").innerHTML = "Time-Up";
-    }
-  }
-}
+        }
 
-function refresh() {
+        function checkNumberPrime(){
 
-  setInterval(myFunction,200)
+            if(number === 2) {
+                var result = "The number is Not a prime.";
+                document.getElementById("display2").innerHTML = result;
 
-}
+            }else if (number % 2 == 0){
+                var result = "The number is Not a prime.";
+                document.getElementById("display2").innerHTML = result;
+            }else {
+
+                var result = "The number is a prime";
+                document.getElementById("display2").innerHTML = result;
+
+            }
+            }
+        
+        function reset() {
+            number = 0;
+            document.getElementById("display").innerHTML = "Odd/Even";
+            document.getElementById("display2").innerHTML = "Prime/Not Prime"
+            document.getElementById("clickCount").innerHTML = " ";
+
+        }
